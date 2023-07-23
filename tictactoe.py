4e1c59c021045a5bed0e65ef1f3beeb65504971e
NateGaNe <81611522+NathanKong06@users.noreply.github.com>
2023-07-23 11:38:17 -0700

    Update Draw Function
    Increased font size of X, simplified code

--- tictactoe.py
+++ tictactoe.py
@@ -4,64 +4,55 @@ from tkinter import *
 board = [[0,0,0],[0,0,0],[0,0,0],[0,0,0]] #Create 4 by 4 Board for board and reset button
 buttons = [[0,0,0],[0,0,0],[0,0,0],[0,0,0]] #Create corresponding buttons for each box
 
-def draw(root,i,j):
+def draw(i,j):
     global buttons
     if i == 0 and j == 0:
-        buttons[0][0] = Button(root, text = 'X', height = 5, width = 10, relief = SOLID, borderwidth = 0.5, command = lambda:draw(root,0,0))
-        buttons[0][0].grid(row = 0, column = 0, sticky = "nsew", padx = (20,0), pady = (20,0))
+        buttons[0][0].configure(text = 'X', font = ("Arial",70))
     elif i == 0 and j == 1:
-        buttons[0][1] = Button(root, text = 'X', height = 5, width = 10, relief = SOLID, borderwidth = 0.5, command = lambda:draw(root,0,1))
-        buttons[0][1].grid(row = 0, column = 1, sticky = "nsew", pady = (20,0))
+        buttons[0][1].configure(text = 'X', font = ("Arial",70))    
     elif i == 0 and j == 2:
-        buttons[0][2] = Button(root, text = 'X', height = 5, width = 10, relief = SOLID, borderwidth = 0.5, command = lambda:draw(root,0,2))
-        buttons[0][2].grid(row = 0, column = 2, sticky = "nsew", padx = (0,20), pady = (20,0))
+        buttons[0][2].configure(text = 'X', font = ("Arial",70))
     elif i == 1 and j == 0:
-        buttons[1][0] = Button(root, text = 'X', height = 5, width = 10, relief = SOLID, borderwidth = 0.5, command = lambda:draw(root,1,0))
-        buttons[1][0].grid(row = 1, column = 0, sticky = "nsew", padx = (20,0))
+        buttons[1][0].configure(text = 'X', font = ("Arial",70)) 
     elif i == 1 and j == 1:
-        buttons[1][1] = Button(root, text = 'X', height = 5, width = 10, relief = SOLID, borderwidth = 0.5, command = lambda:draw(root,1,1))
-        buttons[1][1].grid(row = 1, column = 1, sticky = "nsew")
+        buttons[1][1].configure(text = 'X', font = ("Arial",70))
     elif i == 1 and j == 2:
-        buttons[1][2] = Button(root, text = 'X', height = 5, width = 10, relief = SOLID, borderwidth = 0.5, command = lambda:draw(root,1,2))
-        buttons[1][2].grid(row = 1, column = 2, sticky = "nsew", padx = (0,20))
+        buttons[1][2].configure(text = 'X', font = ("Arial",70)) 
     elif i == 2 and j == 0:
-        buttons[2][0] = Button(root, text = 'X', height = 5, width = 10, relief = SOLID, borderwidth = 0.5, command = lambda:draw(root,2,0))
-        buttons[2][0].grid(row = 2, column = 0, sticky = "nsew", padx = (20,0))
+        buttons[2][0].configure(text = 'X', font = ("Arial",70))
     elif i == 2 and j == 1:
-        buttons[2][1] = Button(root, text = 'X', height = 5, width = 10, relief = SOLID, borderwidth = 0.5, command = lambda:draw(root,2,1))
-        buttons[2][1].grid(row = 2, column = 1, sticky = "nsew")
+        buttons[2][1].configure(text = 'X', font = ("Arial",70))
     elif i == 2 and j == 2:
-        buttons[2][2] = Button(root, text = 'X', height = 5, width = 10, relief = SOLID, borderwidth = 0.5, command = lambda:draw(root,2,2))
-        buttons[2][2].grid(row = 2, column = 2, sticky = "nsew", padx = (0,20))
+        buttons[2][2].configure(text = 'X', font = ("Arial",70))
 
 def create_button_paddings(root):
     global buttons
 
-    buttons[0][0] = Button(root, text = '', height = 5, width = 10, relief = SOLID, borderwidth = 0.5, command = lambda:draw(root,0,0))
+    buttons[0][0] = Button(root, text = '', height = 5, width = 10, font = ("Arial",70), relief = SOLID, borderwidth = 0.5, command = lambda:draw(0,0))
     buttons[0][0].grid(row = 0, column = 0, sticky = "nsew", padx = (20,0), pady = (20,0))
 
-    buttons[0][1] = Button(root, text = '', height = 5, width = 10, relief = SOLID, borderwidth = 0.5, command = lambda:draw(root,0,1))
+    buttons[0][1] = Button(root, text = '', height = 5, width = 10, font = ("Arial",70), relief = SOLID, borderwidth = 0.5, command = lambda:draw(0,1))
     buttons[0][1].grid(row = 0, column = 1, sticky = "nsew", pady = (20,0))
 
-    buttons[0][2] = Button(root, text = '', height = 5, width = 10, relief = SOLID, borderwidth = 0.5, command = lambda:draw(root,0,2))
+    buttons[0][2] = Button(root, text = '', height = 5, width = 10, font = ("Arial",70), relief = SOLID, borderwidth = 0.5, command = lambda:draw(0,2))
     buttons[0][2].grid(row = 0, column = 2, sticky = "nsew", padx = (0,20), pady = (20,0))
 
-    buttons[1][0] = Button(root, text = '', height = 5, width = 10, relief = SOLID, borderwidth = 0.5, command = lambda:draw(root,1,0))
+    buttons[1][0] = Button(root, text = '', height = 5, width = 10, font = ("Arial",70), relief = SOLID, borderwidth = 0.5, command = lambda:draw(1,0))
     buttons[1][0].grid(row = 1, column = 0, sticky = "nsew", padx = (20,0))
 
-    buttons[1][1] = Button(root, text = '', height = 5, width = 10, relief = SOLID, borderwidth = 0.5, command = lambda:draw(root,1,1))
+    buttons[1][1] = Button(root, text = '', height = 5, width = 10, font = ("Arial",70), relief = SOLID, borderwidth = 0.5, command = lambda:draw(1,1))
     buttons[1][1].grid(row = 1, column = 1, sticky = "nsew")
 
-    buttons[1][2] = Button(root, text = '', height = 5, width = 10, relief = SOLID, borderwidth = 0.5, command = lambda:draw(root,1,2))
+    buttons[1][2] = Button(root, text = '', height = 5, width = 10, font = ("Arial",70), relief = SOLID, borderwidth = 0.5, command = lambda:draw(1,2))
     buttons[1][2].grid(row = 1, column = 2, sticky = "nsew", padx = (0,20))
 
-    buttons[2][0] = Button(root, text = '', height = 5, width = 10, relief = SOLID, borderwidth = 0.5, command = lambda:draw(root,2,0))
+    buttons[2][0] = Button(root, text = '', height = 5, width = 10, font = ("Arial",70), relief = SOLID, borderwidth = 0.5, command = lambda:draw(2,0))
     buttons[2][0].grid(row = 2, column = 0, sticky = "nsew", padx = (20,0))
 
-    buttons[2][1] = Button(root, text = '', height = 5, width = 10, relief = SOLID, borderwidth = 0.5, command = lambda:draw(root,2,1))
+    buttons[2][1] = Button(root, text = '', height = 5, width = 10, font = ("Arial",70),relief = SOLID, borderwidth = 0.5, command = lambda:draw(2,1))
     buttons[2][1].grid(row = 2, column = 1, sticky = "nsew")
 
-    buttons[2][2] = Button(root, text = '', height = 5, width = 10, relief = SOLID, borderwidth = 0.5, command = lambda:draw(root,2,2))
+    buttons[2][2] = Button(root, text = '', height = 5, width = 10, font = ("Arial",70), relief = SOLID, borderwidth = 0.5, command = lambda:draw(2,2))
     buttons[2][2].grid(row = 2, column = 2, sticky = "nsew", padx = (0,20))
     #Paddings to top, right, and left sides
 
