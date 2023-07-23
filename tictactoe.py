5ef6ca5ed05f31ea717077ebded9a1ba82e33a5f
NateGaNe <81611522+NathanKong06@users.noreply.github.com>
2023-07-23 16:49:16 -0700

    Colorings
    Background and button colorings and some minor changes that makes editing some features easier

--- tictactoe.py
+++ tictactoe.py
@@ -33,7 +33,7 @@ def check_win_or_draw(root):
         messagebox.showinfo("You win!",f"Player {board[0][2]} wins!")
         reset_board(root)
     elif 0 not in board[0] and 0 not in board[1] and 0 not in board[2]:
-        messagebox.showinfo("Tie!",f"The game is a tie!")
+        messagebox.showinfo("Tie!",f"The game is a tie! 👔")
         reset_board(root)
 
 def draw(root,i,j):
@@ -51,36 +51,39 @@ def draw(root,i,j):
 
 def create_button_paddings(root):
     global buttons
-
-    buttons[0][0] = Button(root, text = '', height = 5, width = 10, font = ("Arial",80), relief = SUNKEN, borderwidth = 1, command = lambda:draw(root,0,0))
+    button_color = "#AFEEEE"
+    h = 5
+    w = 10
+    f = ("Arial",80)
+    buttons[0][0] = Button(root, text = '', height = h, width = w, font = f, bg = button_color, relief = SUNKEN, borderwidth = 1, command = lambda:draw(root,0,0))
     buttons[0][0].grid(row = 0, column = 0, padx = (20,0), pady = (20,0))
 
-    buttons[0][1] = Button(root, text = '', height = 5, width = 10, font = ("Arial",80), relief = SUNKEN, borderwidth = 1, command = lambda:draw(root,0,1))
+    buttons[0][1] = Button(root, text = '', height = h, width = w, font = f, bg = button_color, relief = SUNKEN, borderwidth = 1, command = lambda:draw(root,0,1))
     buttons[0][1].grid(row = 0, column = 1, sticky = "nsew", pady = (20,0))
 
-    buttons[0][2] = Button(root, text = '', height = 5, width = 10, font = ("Arial",80), relief = SUNKEN, borderwidth = 1, command = lambda:draw(root,0,2))
+    buttons[0][2] = Button(root, text = '', height = h, width = w, font = f, bg = button_color, relief = SUNKEN, borderwidth = 1, command = lambda:draw(root,0,2))
     buttons[0][2].grid(row = 0, column = 2, sticky = "nsew", padx = (0,20), pady = (20,0))
 
-    buttons[1][0] = Button(root, text = '', height = 5, width = 10, font = ("Arial",80), relief = SUNKEN, borderwidth = 1, command = lambda:draw(root,1,0))
+    buttons[1][0] = Button(root, text = '', height = h, width = w, font = f, bg = button_color, relief = SUNKEN, borderwidth = 1, command = lambda:draw(root,1,0))
     buttons[1][0].grid(row = 1, column = 0, sticky = "nsew", padx = (20,0))
 
-    buttons[1][1] = Button(root, text = '', height = 5, width = 10, font = ("Arial",80), relief = SUNKEN, borderwidth = 1, command = lambda:draw(root,1,1))
+    buttons[1][1] = Button(root, text = '', height = h, width = w, font = f, bg = button_color, relief = SUNKEN, borderwidth = 1, command = lambda:draw(root,1,1))
     buttons[1][1].grid(row = 1, column = 1, sticky = "nsew")
 
-    buttons[1][2] = Button(root, text = '', height = 5, width = 10, font = ("Arial",80), relief = SUNKEN, borderwidth = 1, command = lambda:draw(root,1,2))
+    buttons[1][2] = Button(root, text = '', height = h, width = w, font = f, bg = button_color, relief = SUNKEN, borderwidth = 1, command = lambda:draw(root,1,2))
     buttons[1][2].grid(row = 1, column = 2, sticky = "nsew", padx = (0,20))
 
-    buttons[2][0] = Button(root, text = '', height = 5, width = 10, font = ("Arial",80), relief = SUNKEN, borderwidth = 1, command = lambda:draw(root,2,0))
+    buttons[2][0] = Button(root, text = '', height = h, width = w, font = f, bg = button_color, relief = SUNKEN, borderwidth = 1, command = lambda:draw(root,2,0))
     buttons[2][0].grid(row = 2, column = 0, sticky = "nsew", padx = (20,0))
 
-    buttons[2][1] = Button(root, text = '', height = 5, width = 10, font = ("Arial",80),relief = SUNKEN, borderwidth = 1, command = lambda:draw(root,2,1))
+    buttons[2][1] = Button(root, text = '', height = h, width = w, font = f, bg = button_color, relief = SUNKEN, borderwidth = 1, command = lambda:draw(root,2,1))
     buttons[2][1].grid(row = 2, column = 1, sticky = "nsew")
 
-    buttons[2][2] = Button(root, text = '', height = 5, width = 10, font = ("Arial",80), relief = SUNKEN, borderwidth = 1, command = lambda:draw(root,2,2))
+    buttons[2][2] = Button(root, text = '', height = h, width = w, font = f, bg = button_color, relief = SUNKEN, borderwidth = 1, command = lambda:draw(root,2,2))
     buttons[2][2].grid(row = 2, column = 2, sticky = "nsew", padx = (0,20))
     #Paddings to top, right, and left sides
 
-    buttons[3][2] = Button(root, text = 'Reset', height = 3, width = 10, relief = SOLID, borderwidth=0.5, command = lambda:reset_board(root))
+    buttons[3][2] = Button(root, text = 'Reset', height = 3, width = 10, relief = SOLID, bg = "#F0FFF0", borderwidth=1, command = lambda:reset_board(root))
     buttons[3][2].grid(row = 3, column = 2, sticky = "ew", pady = 20, padx = (0,10))
     #Reset button
 
@@ -99,6 +102,7 @@ def create_gui():
     root.geometry("700x700") #Default Window Size
     root.resizable(True,True) #Resizeable window
     root.minsize(400,400) #Minimum window size
+    root.configure(background='#E6E6FA')
 
     #Relative weights (Equal in this case) for each row and column
     #Weight is scale for distributing space (default is 0 meaning don't grow if extra space)
